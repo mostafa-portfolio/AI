6150917a371b9613a9f38d1931dc74ee85e3e087
Mostafa <Mo.Mohammed@nu.edu.eg>
2026-04-28 20:34:04 +0300

    v1.1

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,9 +1,9 @@
-
+import logo from './assets/logo.ico'
 
 export default function Header({state,loader,reset}){
     return (
         <header className={loader||state?"logo logo-search-phase":"logo"} onClick={reset}>
-            <img src="src\assets\logo.ico" alt="Logo" className={loader||state?"logo-img logo-img-search-phase":"logo-img "}/>
+            <img src={logo} alt="Logo" className={loader||state?"logo-img logo-img-search-phase":"logo-img "}/>
             <h1 className={loader||state?"hidden":null}>Mostafa</h1>
         </header>
     )
